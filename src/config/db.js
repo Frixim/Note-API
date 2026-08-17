1fdbcd73a64b3ee38fd5be046590cea53999339e
Jimoh Lukman <jaylukmann@gmail.com>
2026-08-17 22:35:18 +0100

    Refactored the middlewares

--- src/config/db.js
+++ src/config/db.js
@@ -4,7 +4,7 @@ const connectDB = async () => {
   try {
     await mongoose.connect(process.env.MONGODB_URI);
 
-    console.log("Blog-API connected to MongoDB successfully");
+    console.log("Note-API connected to MongoDB successfully");
   } catch (error) {
     console.log("MongoDB connection failed:", error);
     process.exit(1);
---
+++ src/middlewares/noteValidators.js
@@ -0,0 +1,45 @@
+//Validation middleware
+
+//import joi for validation
+const Joi = require('joi');
+
+
+//use joi to validate the request body
+const createNoteValidator = (req, res, next) => {
+  const schema = Joi.object({
+    title: Joi.string().min(5).required(),
+    content: Joi.string().min(30).required(),
+    category: Joi.string().optional().valid('work','school','personal','home','club'),
+    tags: Joi.array().items(Joi.string()).min(1).optional()        
+   
+  });
+  const { error} = schema.validate(req.body);
+  if (error) {
+    return res.status(400).json({ error: error.details[0].message });
+  }
+  next();
+};
+
+//use joi to validate the request body
+const updateNoteValidator = (req, res, next) => {
+  const schema = Joi.object({
+    title: Joi.string().min(5),
+    content: Joi.string().min(30),
+    category: Joi.string().valid('work','school','personal','home','club').optional(),
+    tags: Joi.array().items(Joi.string()) 
+   
+  }).min(1);
+
+  const { error} = schema.validate(req.body);
+  if (error) {
+    return res.status(400).json({ error: error.details[0].message });
+  }
+  next();
+};
+
+
+module.exports = {
+  createNoteValidator,
+  updateNoteValidator,
+};
+
